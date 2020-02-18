5eb8ebceae826dcfd6145542906bada900727235
PoojaPadamwar <51482926+PoojaPadamwar@users.noreply.github.com>
2020-02-18 18:04:23 +0530

    Update EmployeeRepository.cs

--- Repository/EmployeeRepository.cs
+++ Repository/EmployeeRepository.cs
@@ -1,4 +1,4 @@
-﻿using Dapper;
+using Dapper;
 using DapperInAsyncWay.Model;
 using DapperInAsyncWay.RepositoryContract;
 using Microsoft.Extensions.Configuration;
@@ -32,10 +32,15 @@ namespace DapperInAsyncWay.Repository
         {
             using (IDbConnection conn = Connection)
             {
-                string sQuery = "SELECT ID, FirstName, LastName, DateOfBirth FROM Employee WHERE ID = @ID";
+                string sQuery = "SELECT ID, FirstName, LastName, DateOfBirth FROM Employee WHERE ID = @ID; Select FullAddress, AddID ,EmpID from Address WHERE EmpID = @ID";
                 conn.Open();
-                var result = await conn.QueryAsync<Employee>(sQuery, new { ID = id });
-                return result.FirstOrDefault();
+
+                var result = await conn.QueryMultipleAsync(sQuery, new { ID = id });
+
+                var emp=result.Read<Employee>().Single();
+                var add = result.Read<Address>();
+                emp.EmpAddress = add.ToList();
+                return emp;
             }
         }
 
@@ -43,10 +48,28 @@ namespace DapperInAsyncWay.Repository
         {
             using (IDbConnection conn = Connection)
             {
-                string sQuery = "SELECT ID, FirstName, LastName, DateOfBirth FROM Employee ";
+                string sQuery = "SELECT ID, FirstName, LastName, DateOfBirth, FullAddress , AddID ,EmpID from Employee inner join Address on Employee.ID= Address.EmpID ";
                 conn.Open();
-                var result = await conn.QueryAsync<Employee>(sQuery);
-                return result.ToList();
+                
+                var empDictionary = new Dictionary<int, Employee>();              
+
+                var list = conn.Query<Employee, Address, Employee>(
+                sQuery,
+                (emp, add) =>
+                {
+                    if (!empDictionary.TryGetValue(emp.ID, out Employee empEntry))
+                    {
+                        empEntry = emp;
+                        empEntry.EmpAddress = new List<Address>();
+                        empDictionary.Add(empEntry.ID, empEntry);
+                    }
+                    empEntry.EmpAddress.Add(add);
+                    return empEntry;
+                },
+                splitOn: "AddID").Distinct();               
+           
+                
+                return list.ToList();
             }
         }
 
